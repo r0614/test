0963fe9fa2a80bcdf0884abfd52b90dddb3e4495
r0614 <60746640+r0614@users.noreply.github.com>
2020-08-18 12:43:04 +0530

    second commit

--- renu/wordc/views.py
+++ renu/wordc/views.py
@@ -6,6 +6,6 @@ import operator
 def home(requests):
     return HttpResponse('<h1> This is first page</h1>')
 def about(requests):
-    return HttpResponse('<h1> This is about</h1>')
+    return HttpResponse('<h1>ABOUT</h1><ul><li> This is about me</li><li> Im Renu Aakanksha from Vasavi College of Engineering</li></ol>')
 def hobbies(requests):
-    return HttpResponse('<h1> This is hobbies</h1>')
+    return HttpResponse('<h1> THESE ARE MY HOBBIES</h1><ul><li> I like to sing</li><li> I like to paint</li></ol>')
